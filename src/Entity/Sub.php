d633f7ea953e30935499b92f3a05129504dd4753
ityt <imran.tierce@gmail.com>
2020-08-26 18:41:28 +0200

    New entity : Sub

---
+++ src/Entity/Sub.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SubRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SubRepository::class)
+ */
+class Sub
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $channel;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $keywords;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getChannel(): ?string
+    {
+        return $this->channel;
+    }
+
+    public function setChannel(string $channel): self
+    {
+        $this->channel = $channel;
+
+        return $this;
+    }
+
+    public function getKeywords(): ?string
+    {
+        return $this->keywords;
+    }
+
+    public function setKeywords(string $keywords): self
+    {
+        $this->keywords = $keywords;
+
+        return $this;
+    }
+}
